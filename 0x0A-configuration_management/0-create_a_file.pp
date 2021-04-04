9fcaf84e9b9b7656dd0f6261c81361db494cc8ab
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-04-04 02:14:05 -0500

    00 | file create | error: Failed to det group to \'33\'

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# file create Puppet maniefest
+file { '/tmp/holberton' :
+    ensure  => file,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
